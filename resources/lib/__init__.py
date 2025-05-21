5867c9d3d95ce45b667eeab79fe0db4cfbad2d90
turbo-potato <koltnapier2014@gmail.com>
2025-05-20 20:36:14 -0400

    Update __init__.py

--- resources/lib/__init__.py
+++ resources/lib/__init__.py
@@ -1,1 +1,13 @@
+import xbmcgui, xbmcplugin, sys
+from youtube_api import YouTubeAPI
 
+def main():
+    api = YouTubeAPI("AIzaSyCKhnfWDTcn3jQIY7mqr06gChhJj313P5s")  # Replace with your key!
+    videos = api.get_popular_videos()
+    for video in videos:
+        li = xbmcgui.ListItem(video["title"])
+        xbmcplugin.addDirectoryItem(int(sys.argv[1]), video["url"], li)
+    xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+if __name__ == "__main__":
+    main()
